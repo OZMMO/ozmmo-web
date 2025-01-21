6aa7efa1ae45235c6f9d199d0d93208592e123bc
ANEUDY ABREU <aneudy91@gmail.com>
2025-01-20 22:14:59 -0600

    quita fuente Google font inter

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import Direccion from "@/components/direccion";
+
 export default async function Page() {
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <Direccion />
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
