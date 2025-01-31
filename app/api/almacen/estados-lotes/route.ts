9782b801a72b30261c6db2647377078e53831eea
ANEUDY ABREU <aneudy91@gmail.com>
2025-01-31 01:46:43 -0600

    Api EstadoLote

--- app/api/almacen/estados-lotes/route.ts
+++ app/api/almacen/estados-lotes/route.ts
@@ -1,13 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from "next/server";
 import { EstadoLoteModel } from "@/lib/db/almacen/estados-lote/estado-lote.model";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
+export async function GET(request: Request) {
+  try {
+    const estadoLoteModel = new EstadoLoteModel();
+    const estadosLote = await estadoLoteModel.findMany();
+    return NextResponse.json(estadosLote);
+  } catch (error) {
+    return NextResponse.json({ error: 'Error fetching estados de lotes' }, { status: 500 });
   }
-
-  const estadoLoteModel = new EstadoLoteModel();
-  const estadosLote = await estadoLoteModel.findMany();
-  // return NextResponse.json(estadosLote);
-  return res.status(200).json(estadosLote);
 }
