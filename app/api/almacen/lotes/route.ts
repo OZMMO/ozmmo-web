a1944ca95167026cb6f0b9d30754ba89a254f9cb
ANEUDY ABREU <aneudy91@gmail.com>
2025-01-31 01:42:14 -0600

    api route get

--- app/api/almacen/lotes/route.ts
+++ app/api/almacen/lotes/route.ts
@@ -4,7 +4,7 @@ import { Lote } from "@/lib/db/almacen/lotes/lote";
 import { CriteriaSqlServer } from "@/lib/db";
 import { auth } from "@/auth";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
--- app/api/direcciones/route.ts
+++ app/api/direcciones/route.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { DireccionModel } from "@/lib/db/sat/direcciones/direccion.model";
 
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
